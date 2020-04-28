abbd7ab68171a676d56ee81c1257071f9ecba098
mr-rampage <readonly@che.wbac.ca>
2020-04-27 21:40:31 -0400

    :sparkles: refactored to completable futures

--- src/main/kotlin/com/example/starter/MainVerticle.kt
+++ src/main/kotlin/com/example/starter/MainVerticle.kt
@@ -1,36 +1,57 @@
 package com.example.starter
 
-import io.vertx.core.Future
+import io.vertx.core.AsyncResult
+import io.vertx.core.Handler
+import io.vertx.core.Promise
+import io.vertx.core.http.HttpServer
+import io.vertx.ext.web.Router
 import io.vertx.ext.web.api.contract.openapi3.OpenAPI3RouterFactory
-import io.vertx.kotlin.core.eventbus.completionHandlerAwait
-import io.vertx.kotlin.core.http.listenAwait
 import io.vertx.kotlin.coroutines.CoroutineVerticle
-import io.vertx.kotlin.coroutines.await
 import io.vertx.serviceproxy.ServiceBinder
+import java.util.concurrent.CompletableFuture
 
 class MainVerticle : CoroutineVerticle() {
 
-  override suspend fun start() {
-    val routerFactoryFuture = Future.future<OpenAPI3RouterFactory>()
-    OpenAPI3RouterFactory.create(vertx, "conduit.yaml", routerFactoryFuture)
-    val routerFactory = routerFactoryFuture.await()
+  override fun start(startPromise: Promise<Void>) {
+    val routerFactoryFuture = CompletableFuture<OpenAPI3RouterFactory>()
+    OpenAPI3RouterFactory.create(vertx, "conduit.yaml", withPromise(routerFactoryFuture))
 
-    try {
-      val router = routerFactory.mountServicesFromExtensions().router
+    val serviceBinder = ServiceBinder(vertx)
+      .setAddress("pet.service")
+      .registerLocal(PetService::class.java, PetServiceImpl())
 
-      vertx
-        .createHttpServer()
-        .requestHandler(router)
-        .listenAwait(8080)
+    routerFactoryFuture
+      .thenApply(this::createRouter)
+      .thenApply(this::startHttpServer)
+      .thenAccept {
+        serviceBinder.completionHandler(startPromise)
+        println("Started server on port 8080")
+      }
+      .exceptionally {
+        println("Unable to start server")
+        startPromise.fail(it.cause)
+        null
+      }
+  }
 
-      ServiceBinder(vertx)
-        .setAddress("pet.service")
-        .registerLocal(PetService::class.java, PetServiceImpl())
-        .completionHandlerAwait()
-
-      println("Started server on port 8080")
-    } catch (e: Exception) {
-      println("Unable to start server")
+  private fun<T> withPromise(future: CompletableFuture<T>): Handler<AsyncResult<T>> {
+    return Handler {
+      if (it.succeeded()) {
+        future.complete(it.result())
+      } else {
+        future.completeExceptionally(it.cause())
+      }
     }
+  }
+
+  private fun startHttpServer(router: Router) : HttpServer {
+    return vertx
+      .createHttpServer()
+      .requestHandler(router)
+      .listen(8080)
+  }
+
+  private fun createRouter(routerFactory: OpenAPI3RouterFactory): Router {
+    return routerFactory.mountServicesFromExtensions().router
   }
 }
